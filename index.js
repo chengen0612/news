a120805cc92ff26c348320b4d900f8bf91ab94af
evans <chengen0612@gmail.com>
2022-08-16 08:42:24 +0800

    feat: add routes for getting stories

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ require("dotenv").config();
 const app = express();
 
 const PORT = process.env.PORT;
+const STORIES = require("./stories.json");
 
 app.get("/ping", (_req, res) => {
   res.send("pong");
+});
+
+app.get("/stories", (_req, res) => {
+  res.json(STORIES);
+});
+
+app.get("/stories/:title", (req, res) => {
+  const { title } = req.params;
+  const regex = new RegExp(title, "gi");
+
+  res.send(STORIES.filter((STORY) => regex.test(STORY.title)));
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
